a12b07535355d479549fffc278a78ff5784a173b
Kaushik Malvadkar <kaushikmalvadkar7@gmail.com>
2017-07-30 12:07:29 +0530

    DataStructure
    If Else

---
+++ DataStructure.py
@@ -0,0 +1,29 @@
+s=(10,20,30)
+s.index(10)
+s.count(s)
+l1=[10,20,30,40]
+d={'one':1,'two':2,'three':3}
+dir(l1)
+#l1.append(10)
+#print(l1)
+#l1.pop(3)
+#l1.insert(4,10)
+print(l1)
+#d['two']
+#del d['one']
+print(d)
+
+name ="Tixdo"
+if name in ["Tixdo","1"]:
+    print("Welcome To ...")
+elif name=="Tixdo":
+    pass
+else:
+    print("Still Welcome :)")
+
+if name in {"Tixdo":"1"}:
+    print("Welcome To ...")
+elif name=="Tixdo":
+    pass
+else:
+    print("Still Welcome :)")
